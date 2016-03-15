e31b6def9a521f55f0ab96b0bc20735a1d3d91de
John A Seubert <>
2016-03-15 01:07:34 -0700

    Automated commit at 2016-03-15 08:07:34 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -17,10 +17,16 @@ $(function() {
             $(".player-header").removeClass("sticky-nav-enabled");
         }
 });*/
+
+function workingMonths() {
+    var months = new Date().getMonth() + 3;
+    return "2 years, " + months + " months";
+}
+
 $(document).ready(function () {
   var playerHeader = $("#player-header");
   var scroll = $(window).scrollTop();
-  console.log(scroll + " " + playerHeader.height());
+  //console.log(scroll + " " + playerHeader.height());
   if (scroll >= (playerHeader.height() )) {
       playerHeader.addClass("sticky-nav-enabled");
   } else {
@@ -30,11 +36,36 @@ $(document).ready(function () {
   window.onscroll = function (e) {
   // called when the window is scrolled.
     var scroll = $(window).scrollTop();
-    console.log(scroll + " " + playerHeader.height());
+  //  console.log(scroll + " " + playerHeader.height());
     if (scroll >= (playerHeader.height() )) {
         playerHeader.addClass("sticky-nav-enabled");
     } else {
         playerHeader.removeClass("sticky-nav-enabled");
     }
   }
+
+  $("#section_nav").click(function (event) {
+
+      $("li", this)
+      .removeClass("active")
+      .filter(event.target)
+      .addClass("active");
+
+      var value = $(event.target).attr('id');
+
+      //hide all stats
+      $(".stat-panel").hide();
+
+      //show the one that was tapped
+      if(value.localeCompare("majorsTab") == 0) {
+        $("#majorsPanel").show();
+      } else if(value.localeCompare("minorsTab") == 0) {
+        $("#minorsPanel").show();
+      } else if(value.localeCompare("collegeTab") == 0) {
+        $("#collegePanel").show();
+      } else if(value.localeCompare("scoutingTab") == 0) {
+        $("#scoutingPanel").show();
+      }
+
+  })
 });
